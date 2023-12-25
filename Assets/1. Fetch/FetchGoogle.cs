0e11ccae3e2b4563ced7a10f85eebbe8d1162ccc
Wiviart <dungphamhuu95@gmail.com>
2023-12-25 11:24:53 +0700

    fix

--- Assets/1. Fetch/FetchGoogle.cs
+++ Assets/1. Fetch/FetchGoogle.cs
@@ -1,12 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 public class FetchGoogle : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject textContainer;
+
     IEnumerator Start()
     {
         string url = "https://www.google.com/";
-     
+
         UnityWebRequest request = UnityWebRequest.Get(url);
 
         yield return request.SendWebRequest();
@@ -14,10 +20,30 @@ public class FetchGoogle : MonoBehaviour
         if (request.result == UnityWebRequest.Result.Success)
         {
             Debug.Log(request.downloadHandler.text);
+
+            string html = request.downloadHandler.text;
+
+            string[] splits = SplitContent(html, 1000);
+
+            foreach (string split in splits)
+            {
+                TextMeshProUGUI newText = Instantiate(text, textContainer.transform);
+                newText.text = split;
+            }
         }
         else
         {
             Debug.Log(request.error);
         }
+    }
+
+    private string[] SplitContent(string content, int chunkSize)
+    {
+        List<string> chunks = new List<string>();
+        for (int i = 0; i < content.Length; i += chunkSize)
+        {
+            chunks.Add(content.Substring(i, Mathf.Min(chunkSize, content.Length - i)));
+        }
+        return chunks.ToArray();
     }
 }
